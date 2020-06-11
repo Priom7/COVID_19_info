626286e53f428d63e1941e25cc802d7093696cd2
Md. Sharif Alam <priom7197@gmail.com>
2020-06-11 21:54:21 +0600

    Images Updated (TODO:need to fix installable requirements)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Cards, Charts, CountryPicker} from './components'
+import styles from './App.module.css'
+import { fetchData } from './api'
+import logo from './images/Logo.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        data : {},
+        country : ''
+    }
+
+    async componentDidMount(){
+        const fetchdata = await fetchData()
+        this.setState({data : fetchdata})
+    }
+
+    countryChangeHandler = async (country)=>{
+        const fetchdata = await fetchData(country)
+        this.setState({data : fetchdata, country: country})
+
+    }
+    render() {
+
+        const  { data, country } = this.state;
+        return(
+            <div className={styles.container}>
+            <img className={styles.image} src={logo} alt="COVID-19 info."/>
+            <h3>Search info by Country </h3>
+                <CountryPicker countryChangeHandler={this.countryChangeHandler}/>
+                <Cards data = {data}/>
+                <Charts data = {data} country={country}/>
+
+                <footer className="footer">This App was developed by Md. Sharif Alam 
+                <a href="https://github.com/Priom7"> GitHub </a>/ 
+                <a href="https://www.linkedin.com/in/md-sharif-alam/"> LinkedIn </a> </footer>
+            </div>
+        )
+    }
 }
 
 export default App;
